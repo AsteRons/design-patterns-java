2ec9106446cbea762b4df8db8c9971ba4a74f9f3
Maciej Rudnik <maciej.rudnik2@gmail.com>
2025-07-01 23:24:10 +0200

    test(chain-of-responsibility): add unit tests for chain-of-responsibility

--- behavioral/cor/src/test/java/com/authchain/AppTest.java
+++ behavioral/cor/src/test/java/com/authchain/AppTest.java
@@ -1,7 +1,57 @@
 package com.authchain;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import com.authchain.handler.Handler;
+import com.authchain.model.Request;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.authchain.handler.*;
 
 class AppTest {
+
+    private Handler handlerChain;
+
+    @BeforeEach
+    void setUp() {
+        handlerChain = new EmailValidator();
+        handlerChain.setNext(new PasswordValidator())
+                .setNext(new UserExistenceChecker())
+                .setNext(new RoleChecker());
+    }
+
+
+    @Test
+    void testValidRequestPassesAllChecks() {
+        Request request = new Request("john@example.com", "superSecret", true, true);
+        assertTrue(handlerChain.handle(request));
+    }
+
+    @Test
+    void testInvalidEmailFails() {
+        Request request = new Request("invalid-email", "superSecret", true, true);
+        assertFalse(handlerChain.handle(request));
+    }
+
+    @Test
+    void testShortPasswordFails() {
+        Request request = new Request("john@example.com", "123", true, true);
+        assertFalse(handlerChain.handle(request));
+    }
+
+    @Test
+    void testNonExistingUserFails() {
+        Request request = new Request("john@example.com", "superSecret", false, true);
+        assertFalse(handlerChain.handle(request));
+    }
+
+    @Test
+    void testMissingAdminRoleFails() {
+        Request request = new Request("john@example.com", "superSecret", true, false);
+        assertFalse(handlerChain.handle(request));
+    }
 
 }
